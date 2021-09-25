99c61ce24165beade325ce1a496b742213675308
Yago B. Fontoura <yagofontoura222@yahoo.com>
2021-09-25 17:32:23 -0300

    add BackEnd and modification pages

---
+++ src/controllers/QuestionController.js
@@ -0,0 +1,11 @@
+module.exports = {
+    index(req, res){
+
+        const roomId = req.params.room;
+        const questionId = req.params.question;
+        const action = req.params.action;
+        const password = req.body.password;
+
+        
+        console.log(`room = ${roomId}, questitonId = ${questionId}, action = ${action}, password = ${password}`)
+    }}
